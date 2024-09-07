81f44b400d17a3d102b7d6710895e82f3b00453a
PALOMA CRISTIANE <palomacristiane8806@gmail.com>
2024-09-07 20:21:56 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Minha_Plataforma
+ Meu Site Candidato_Certo
---
+++ princ.js
@@ -0,0 +1,10 @@
+document.getElementById('form-candidato').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Dados do candidato enviados com sucesso!');
+});
+
+document.getElementById('form-empregador').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Cadastro do empregador realizado com sucesso!');
+});
+TalentMatch 
